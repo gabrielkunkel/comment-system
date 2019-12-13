2d0231d44b013f93c746cdd7eb467bfb44a602b9
Gabriel Kunkel <henrysaltandson@gmail.com>
2019-12-13 14:29:55 -0600

    should add a comment passing without mongoDB

--- server/test/comments.test.ts
+++ server/test/comments.test.ts
@@ -6,36 +6,24 @@ describe("graphQl comments test", () => {
 
     it("should add a comment", (done) => {
 
-        const query = `mutation CreateComment($input: CommentInput) {
-            createComment(input: $input) {
+        const query = `mutation {
+            createOrUpdateComment(input: {_id: "4", author: "Brian", text: "This is my favorite video of all time."}) {
               _id
               author
               text
             }
           }`;
 
-        const variables = {
-              input: {
-                  _id: "this is id",
-                  author: "Brian",
-                  text: "This is my favorite video of all time."
-              }
-          };
-
-        console.log("stringified JSON:", JSON.stringify({ query, variables}));
-
         request(app)
             .post("/comments")
-            .send(JSON.stringify({ query, variables}))
+            .send({query})
             .expect(200)
             .end((err, res) => {
-                console.log("is there an err? ", err);
                 if (err) { return done(err); }
-                console.log("does this response get through? ", res);
                 // tslint:disable-next-line: no-unused-expression
-                expect(res.body.data.id).to.exist;
-                expect(res.body.data.author).to.equal("Brian");
-                expect(res.body.data.text).to.equal("This is my favorite video of all time.");
+                expect(res.body.data.createOrUpdateComment._id).to.exist;
+                expect(res.body.data.createOrUpdateComment.author).to.equal("Brian");
+                expect(res.body.data.createOrUpdateComment.text).to.equal("This is my favorite video of all time.");
                 done();
             });
     });
